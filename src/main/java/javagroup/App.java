a811f4228eed40892a55c19772f719b80e82f0f8
root <root@ip-172-31-17-59.us-east-2.compute.internal>
2020-05-27 12:04:30 +0000

    test commit from sekhar

---
+++ src/main/java/javagroup/App.java
@@ -0,0 +1,13 @@
+package javagroup;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
